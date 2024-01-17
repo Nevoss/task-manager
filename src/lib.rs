a52f7ffe1cdd951dd28e0f1ec667d20e5e4508e6
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2024-01-17 22:54:34 +0200

    change break lines to trait

--- src/lib.rs
+++ src/lib.rs
@@ -35,6 +35,28 @@ struct DB {
     data: Vec<Task>,
 }
 
+trait TableFormat {
+    fn break_lines_by_word(&self, word_count: usize) -> String;
+}
+
+impl TableFormat for String {
+    fn break_lines_by_word(&self, break_at: usize) -> String {
+        let mut result = String::new();
+
+        for (index, word) in self.split(" ").enumerate() {
+            if index % break_at == 0 {
+                result.push_str("\n");
+            }
+
+            let word = format!("{} ", &word);
+
+            result.push_str(&word);
+        }
+
+        result.trim().to_string()
+    }
+}
+
 pub fn ls() -> Result<(), Box<dyn Error>> {
     let db = read_db()?;
 
@@ -45,8 +67,8 @@ pub fn ls() -> Result<(), Box<dyn Error>> {
     db.data.iter().for_each(|task| {
         table.add_row(vec![
             task.id.to_string(),
-            format_for_table_view(&task.title, 4),
-            format_for_table_view(&task.description, 4),
+            task.title.break_lines_by_word(4),
+            task.description.break_lines_by_word(4),
             task.status.to_string(),
         ]);
 
@@ -81,21 +103,4 @@ fn read_db() -> Result<DB, Box<dyn Error>> {
     let db: DB = serde_json::from_str(json)?;
 
     Ok(db)
-}
-
-fn format_for_table_view(subject: &str, split_at: usize) -> String {
-    let subject = subject.to_string();
-    let mut result = String::new();
-
-    for (index, word) in subject.split(" ").enumerate() {
-        if index % split_at == 0 {
-            result.push_str("\n");
-        }
-
-        let word = format!("{} ", &word);
-
-        result.push_str(&word);
-    }
-
-    result.trim().to_string()
 }
